da5db34e6a0109f05c8b9519ce634f829a21e775
Josue <ahadihjosue@gmail.com>
2024-04-30 12:32:46 +0200

    initial commit

---
+++ default_function_parameters.js
@@ -0,0 +1,10 @@
+function greet(name, greeting) {
+    name = (typeof name !== 'undefined') ? name : 'Student';
+    greeting = (typeof greeting !== 'undefined') ? greeting : 'Welcome';
+
+    return `${greeting} ${name}!`;
+}
+
+greet(); // Welcome Student!
+greet('James'); // Welcome James!
+greet('Richard', 'Howdy'); // Howdy Richard!
